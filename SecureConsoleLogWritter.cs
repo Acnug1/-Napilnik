d37b735da517eae24dfa456e46ada8ce756bfcbb
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-05-08 18:18:48 +0500

    Added task solution

---
+++ SecureConsoleLogWritter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson
+{
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+}
