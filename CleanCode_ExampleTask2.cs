69aafe89e58b9864a8f84e757b4dcb21ac0cb6eb
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-06-19 17:55:03 +0500

    Added task solution

---
+++ CleanCode_ExampleTask2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CleanCodeTask2
+{
+    class CleanCode_ExampleTask2
+    {
+        public static int TryGetElementIndex(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
