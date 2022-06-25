6ed0e1851f90ee2d904974995fafa98fb7347cfb
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-06-25 17:21:24 +0500

    Added task solution

---
+++ CleanCode_ExampleTask17.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CleanCodeTask7
+{
+    class CleanCode_ExampleTask17
+    {
+        private static float _chance;
+        private static int _hourlyRate;
+        private static Random _random = new Random();
+
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void GenerateChance()
+        {
+            _chance = _random.Next(0, 101);
+        }
+
+        public static int CalculateSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
