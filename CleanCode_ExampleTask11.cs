9a7a69b07a5b432e09dced3ac84d92cf66d016d9
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-06-25 15:47:47 +0500

    Added task solution

---
+++ CleanCode_ExampleTask11.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CleanCodeTask4
+{
+    class CleanCode_ExampleTask11
+    {
+        private int _armySize = 10;
+        private int _coins = 10;
+        private string _name = "Vladislav";
+    }
+}
