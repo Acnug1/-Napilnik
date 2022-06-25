c0c750ec3875bb16d919feff732ed2830905cd41
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-06-25 17:37:51 +0500

    Added task solution

---
+++ Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CleanCodeTask8
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+    }
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CleanCodeTask8
+{
+    class PlayerMove
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+    }
+}
