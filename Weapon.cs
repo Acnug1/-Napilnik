6a63667bcdea8c702a2ee56209b05a268d4a4cd5
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-06-25 15:26:19 +0500

    Added task solution

---
+++ Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CleanCodeTask3
+{
+    class Weapon
+    {
+        private const int EmptyMagazine = 0;
+        private const int BulletsPerShot = 1;
+        private int _bullets;
+
+        public bool CanShoot() => _bullets > EmptyMagazine;
+
+        public void Shoot() => _bullets -= BulletsPerShot;
+    }
+}
