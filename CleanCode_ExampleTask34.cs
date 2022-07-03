31047377e2a193d07efdfd227a46a7dea60456b1
acnug1 <acnug0gyBaH4uK@yandex.ru>
2022-07-03 17:22:34 +0500

    Added task solution

---
+++ CleanCode_ExampleTask34.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CleanCodeTask12
+{
+    class CleanCode_ExampleTask34
+    {
+        public void Shoot(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Find(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
